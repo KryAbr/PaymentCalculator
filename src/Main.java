ad059684f9029d0dc1928bc9643fb7692552885c
krab <krystian.abraszkiewicz@mailmix.pl>
2020-03-29 16:59:45 +0200

    Adjustments after code review and I added a list of products with stocks from which user can choose the product for purchase.

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,21 @@
+import javafx.util.Pair;
+
 import java.util.Scanner;
 
 public class Main {
+
+    private static Pair[] STOCK_TABLE = new Pair[]{new Pair(10, "Macbook"), new Pair(0, "Samsung TV"), new Pair(7, "GPS")};
+
     public static void main(String[] args) {
 
        String name = gatherAndCheckName();
        float price = gatherAndCheckPrice();
        int numberOfPayments = gatherAndCheckNumberOfPayments();
        long pesel = gatherAndCheckPesel();
-       calculateAndShowPayments(price, numberOfPayments);
+       calculateAndShowPayments(price, numberOfPayments, name, pesel);
     }
 
-    private static void calculateAndShowPayments(float price, int numberOfPayments){
+    private static void calculateAndShowPayments(float price, int numberOfPayments, String name, long pesel){
 
         float interest = calculateInterest(numberOfPayments);
         float totalPayment = calculateTotalPayment(price, interest);
@@ -20,25 +25,39 @@ public class Main {
         float monthlyInterest = totalInterest / numberOfPayments;
 
         System.out.println();
-        System.out.println("Zakup udany!");
+        System.out.println("-----------------------------------------------------");
+        System.out.println("Zakup udany! Prosimy sprawdź poprawność danych");
+        System.out.println("Twój numer PESEL to " + pesel);
+        System.out.println("Kupiłeś: " + name);
         System.out.println("Twoje miesieczna rata to: " + monthlyPayment + "zl");
         System.out.println("Kapital raty: " + monthlyCapitalPayment + "zl" + " | Odsetki w racie: " + monthlyInterest + "zl");
         System.out.println("Calkowita kwota do splaty to: " + totalPayment + "zl");
         System.out.println("Calkowita kwota odsetek: " + totalInterest + "zl");
+        System.out.println("-----------------------------------------------------");
     }
 
     private static String gatherAndCheckName() {
         Scanner in = new Scanner(System.in);
-        System.out.print("Podaj nazwe towaru: ");
-        String name = in.next().toString();
+        System.out.println();
+        System.out.println("Witamy w sklepie z elektroniką!");
+        System.out.println();
+        listProducts();
+        System.out.println();
+        System.out.print("Wpisz nazwę produktu z listy: ");
+        String name = in.next();
 
+        if(!isInStock(name)){
+            System.out.println("Przepraszam, ale obecnie ma tego produktu.");
+            System.out.println();
+            return gatherAndCheckName();
+        }
         return name;
     }
 
     private static float gatherAndCheckPrice(){
         Scanner in = new Scanner(System.in);
         System.out.print("Podaj cene: ");
-        Float price = in.nextFloat();
+        float price = in.nextFloat();
 
         if(!isValidPrice(price)){
             System.out.println("Nieprawidlowa cena. Podaj cene z przedzialu 1500 - 45000");
@@ -64,33 +83,22 @@ public class Main {
         System.out.print("Podaj pesel: ");
         long pesel = in.nextLong();
 
-        if(!hasCredit(pesel)){
+        if(!isCredible(pesel)){
             System.out.println("Niestety nie posiadasz zdolnosci kredytowej");
             return gatherAndCheckPesel();
         }
-
         return pesel;
     }
 
     private static boolean isValidPrice(float price){
-        if (price >= 1500 && price <= 45000) {
-            return true;
-        }
-        else {
-            return false;
-        }
+        return price >= 1500 && price <= 45000;
     }
 
     private static boolean isValidNumberOfPayments(int numberOfPayments){
-        if(numberOfPayments >= 8 && numberOfPayments <= 48){
-            return true;
-        }
-        else {
-            return false;
-        }
+        return numberOfPayments >= 8 && numberOfPayments <= 48;
     }
 
-    private static boolean hasCredit(long pesel){
+    private static boolean isCredible(long pesel){
 
         long[] badPesels = new long[]{123456, 654321};
         for(int i = 0; i < badPesels.length; i++)
@@ -118,5 +126,24 @@ public class Main {
         float totalPrice = price + price * interest;
 
         return totalPrice;
+    }
+
+    private static boolean isInStock(String product){
+        int tempStock = 0;
+        for(int i = 0; i < STOCK_TABLE.length; i++){
+            Pair tempProduct = STOCK_TABLE[i];
+
+            if (tempProduct.getValue().toString().equalsIgnoreCase(product)){
+                tempStock = (int)tempProduct.getKey();
+            }
+        }
+        return !(tempStock == 0);
+    }
+
+    private static void listProducts() {
+        for(int i = 0; i < STOCK_TABLE.length; i++){
+            Pair product = STOCK_TABLE[i];
+            System.out.println(i + 1 + ". " + product.getValue().toString() + " - Obecny stan w magazynie: " + product.getKey());
+        }
     }
 }
